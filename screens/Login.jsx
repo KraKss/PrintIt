5fa35b10d36ada6d1c870bd0404e6442821762c3
KraKss <97532245+KraKss@users.noreply.github.com>
2025-01-02 11:35:29 +0100

    add .env

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -15,7 +15,6 @@ import {
 import { useDispatch } from 'react-redux';
 import { login } from '../redux/userSlice';
 import { API } from "../utils/API";
-import API_ROUTES from "../utils/indexAPI";
 
 export default function Login({ navigation }) {
     const [email, setEmail] = useState('');
@@ -37,10 +36,10 @@ export default function Login({ navigation }) {
         setIsLoading(true);
 
         try {
-            const response = await API.post(`${API_ROUTES.API_ROUTE}${API_ROUTES.LOGIN_ROUTE}`, { email, password });
+            const response = await API.post(`${process.env.EXPO_PUBLIC_BASE_API_ROUTE}${process.env.EXPO_PUBLIC_LOGIN_ROUTE}`, { email, password });
             const token = response.data;
 
-            const userProfileResponse = await API.get(`${API_ROUTES.API_ROUTE}${API_ROUTES.PROFILE_ROUTE}?email=${email}`, {
+            const userProfileResponse = await API.get(`${process.env.EXPO_PUBLIC_BASE_API_ROUTE}${process.env.EXPO_PUBLIC_PROFILE_ROUTE}?email=${email}`, {
                 headers: { Authorization: `Bearer ${token}` },
             });
 
